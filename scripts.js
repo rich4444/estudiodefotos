4235df7f5f3658fb3d412f5e67200de4ff9c38fc
Ricardo <ricardog.n@hotmail.com>
2022-05-27 15:58:57 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -6,19 +6,23 @@ let autoSlider = setTimeout(plusSlides, 5000);
 showSlides(slideIndex);
 
 //Next and prev controls
-function plusSlides(n = 1){
-    clearTimeout(autoSlider);
-    showSlides(slideIndex += n);
-    autoSlider = setTimeout(plusSlides, 5000)
+function plusSlides(n = 1) {
+  clearTimeout(autoSlider);
+  showSlides((slideIndex += n));
+  autoSlider = setTimeout(plusSlides, 5000);
 }
 
-function showSlides(n){
-    if(n > slides.length) {slideIndex = 1}
-    if(n < 1){slideIndex = slides.length}
-    for(let i = 0; i < slides.length; i++){
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
+function showSlides(n) {
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
 }
 
 //Nav bar buttons
@@ -27,40 +31,39 @@ const closeButton = document.getElementById("closeNavbar");
 const ulList = document.getElementById("navbarList");
 const navLinks = document.querySelectorAll(".navLink");
 
-navLinks.forEach(link => {
-    link.addEventListener("click", () =>{
-        if(window.screen.width < 850)
-            closeNavBar();
-    })
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    if (window.screen.width < 850) closeNavBar();
+  });
 });
 
 openButton.addEventListener("click", () => {
-    closeButton.style.display = "block";
-    ulList.style.display = "block";
-    openButton.style.display = "none";
+  closeButton.style.display = "block";
+  ulList.style.display = "block";
+  openButton.style.display = "none";
 });
 
 closeButton.addEventListener("click", () => {
-    closeNavBar();
+  closeNavBar();
 });
 
-function closeNavBar (){
-    closeButton.style.display = "none";
-    ulList.style.display = "none";
-    openButton.style.display = "block";
+function closeNavBar() {
+  closeButton.style.display = "none";
+  ulList.style.display = "none";
+  openButton.style.display = "block";
 }
 
 //Handle logos and back button
 const logoSmall = document.getElementById("logo-small");
 const backBtn = document.getElementById("backBtn");
 
-window.onscroll = function() {
-    var scrollLimit = 100;
-    if (window.scrollY >= scrollLimit) {
-      logoSmall.style.opacity = 1;
-      backBtn.style.display = "block";
-    } else {
-        logoSmall.style.opacity = 0;
-        backBtn.style.display = "none";
-    }
-  };
+window.onscroll = function () {
+  var scrollLimit = 100;
+  if (window.scrollY >= scrollLimit) {
+    logoSmall.style.opacity = 1;
+    backBtn.style.display = "block";
+  } else {
+    logoSmall.style.opacity = 0;
+    backBtn.style.display = "none";
+  }
+};
